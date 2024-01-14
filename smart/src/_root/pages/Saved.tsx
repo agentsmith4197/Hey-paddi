73b24f31b97d05a1568a99fcf666aceb0a44b7d9
agentsmith4197 <smithmandu@gmail.com>
2024-01-14 18:18:01 +0100

    new

--- smart/src/_root/pages/Saved.tsx
+++ smart/src/_root/pages/Saved.tsx
@@ -10,36 +10,29 @@ const Saved = () => {
     .map((savePost: Models.Document) => ({
       ...savePost.post,
       creator: {
+        $id: currentUser.$id,
         imageUrl: currentUser.imageUrl,
       },
     }))
     .reverse();
 
   return (
-    <div className="saved-container">
-      {/* <div className="flex gap-2 w-full max-w-5xl">
-        <img
-          src="/assets/icons/save.svg"
-          width={36}
-          height={36}
-          alt="edit"
-          className="invert-white"
-        />
-        <h2 className="h3-bold md:h2-bold text-left w-full">Saved Posts</h2>
-      </div> */}
-
+    <>
+      
       {!currentUser ? (
+        <div className="flex-center w-full h-full">
         <Loader />
+      </div>
       ) : (
-        <ul className="w-full flex justify-center max-w-5xl gap-9">
+        <>
           {savePosts.length === 0 ? (
             <p className="text-light-4">No available posts</p>
           ) : (
             <GridPostList posts={savePosts} showStats={false} />
           )}
-        </ul>
+        </>
       )}
-    </div>
+    </>
   );
 };
 
